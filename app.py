09f8dd0353ea65e87ae980d217d41496b8a708b6
Ellen Yu <72773854+eyy8k@users.noreply.github.com>
2021-11-24 17:52:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -80,12 +80,12 @@ mydata = gss_clean[['income', 'sex', 'job_prestige']]
 ## Create new features that breaks job_prestige into six categories with equally sized ranges
 mydata['job_prestige_binned'] = pd.cut(mydata.job_prestige, 6)
 ## Reorder the bins 
-# mydata.job_prestige_binned = mydata.job_prestige_binned.astype('str').astype('category').cat.reorder_categories(['(15.936, 26.667]',
-#                                                                                                                  '(26.667, 37.333]',
-#                                                                                                                  '(37.333, 48.0]',
-#                                                                                                                  '(48.0, 58.667]',
-#                                                                                                                  '(58.667, 69.333]',
-#                                                                                                                  '(69.333, 80.0]'])
+mydata.job_prestige_binned = mydata.job_prestige_binned.astype('str').astype('category').cat.reorder_categories(['(15.936, 26.667]',
+                                                                                                                 '(26.667, 37.333]',
+                                                                                                                 '(37.333, 48.0]',
+                                                                                                                 '(48.0, 58.667]',
+                                                                                                                 '(58.667, 69.333]',
+                                                                                                                 '(69.333, 80.0]'])
 ## Drop rows with missing data
 mydata = mydata.dropna()
 ## Create a facet grid with 3 rows and 2 columns 
